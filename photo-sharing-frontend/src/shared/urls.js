e6f25719d7f66c6c040ea50839474fdd0ae64bd3
Jai Kumar Kaushil <jaikumarkaushil@gmail.com>
2024-08-10 14:00:02 -0400

    Updated urls.js

--- photo-sharing-frontend/src/shared/urls.js
+++ photo-sharing-frontend/src/shared/urls.js
@@ -2,7 +2,9 @@ const URLS = {
   BASE_URL: 'http://localhost:3000',
   // GraphQL_URL: 'http://127.0.0.1:4000/graphql',
   GraphQL_URL: 'https://jcapture-backend-v2.azurewebsites.net/graphql',
-  PROD_GraphQL_URL: 'https://jcapture-backend-v2.azurewebsites.net/graphql'
+  // PROD_GraphQL_URL: 'https://jcapture-backend-v2.azurewebsites.net/graphql'
+  PROD_GraphQL_URL: 'https://photo-sharing-app-backend-wqru.onrender.com/graphql'
+  
 }
 
 export default URLS;
